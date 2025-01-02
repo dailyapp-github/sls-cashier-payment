6e4fa54ae5e27725a8291c15b7529e8e08ff84cb
macbook pro <macbookpro@macbooks-MacBook-Pro.local>
2025-01-02 09:43:25 +0700

    stoped system

--- src/sls-cashier-create-payment.ts
+++ src/sls-cashier-create-payment.ts
@@ -1,4 +1,8 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
 import { connect } from "../handler/mongooseConnection";
 import callbackResponse from "../handler/callback";
 import Validator from "fastest-validator";
@@ -9,65 +13,74 @@ import createOtpPaymentRequest from "../handler/helper/createOtpPaymentRequest";
 import sendWa from "../handler/helper/waNotificationPayment";
 import getOutletById from "../handler/helper/getOutletById";
 
-export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
-	context.callbackWaitsForEmptyEventLoop === false;
-	await connect(process.env.DB_NAME || "test");
-	try {
-		console.log("YEAY");
-		const body = event.body ? JSON.parse(event.body) : null;
-		if (!body || !body.phoneNumber || !body.outletId || !body.coin) {
-			return callbackResponse(2000, {}, "body request is required", "body request is required");
-		}
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+) => {
+  context.callbackWaitsForEmptyEventLoop === false;
+  await connect(process.env.DB_NAME || "test");
+  try {
+    // console.log("YEAY");
+    // const body = event.body ? JSON.parse(event.body) : null;
+    // if (!body || !body.phoneNumber || !body.outletId || !body.coin) {
+    // 	return callbackResponse(2000, {}, "body request is required", "body request is required");
+    // }
 
-		const phoneFormat: any = phone(`+${body.phoneNumber}`, { country: "ID" });
-		console.log("phoneFormat", phoneFormat);
-		if (phoneFormat.isValid === false) {
-			return callbackResponse(2001, {}, "phone format incorrect", "phone format incorrect");
-		}
+    // const phoneFormat: any = phone(`+${body.phoneNumber}`, { country: "ID" });
+    // console.log("phoneFormat", phoneFormat);
+    // if (phoneFormat.isValid === false) {
+    // 	return callbackResponse(2001, {}, "phone format incorrect", "phone format incorrect");
+    // }
 
-		const user = await getUserByPhone(body.phoneNumber);
-		if (!user) {
-			return callbackResponse(2002, {}, "phone number not registered", "phone number not registered");
-		}
+    // const user = await getUserByPhone(body.phoneNumber);
+    // if (!user) {
+    // 	return callbackResponse(2002, {}, "phone number not registered", "phone number not registered");
+    // }
 
-		if(user.isBanned){
-			return callbackResponse(2004, {}, "user has been banned", "user has been banned");
-		}
+    // if(user.isBanned){
+    // 	return callbackResponse(2004, {}, "user has been banned", "user has been banned");
+    // }
 
-		const outlet = await getOutletById(body.outletId);
-		if (!outlet) {
-			return callbackResponse(2003, {}, "outlet not found", "outlet not found");
-		}
+    // const outlet = await getOutletById(body.outletId);
+    // if (!outlet) {
+    // 	return callbackResponse(2003, {}, "outlet not found", "outlet not found");
+    // }
 
-		const userCoin = await getLastCoin(user.user._id);
-		if (userCoin < body.coin) {
-			return callbackResponse(
-				2004,
-				{},
-				"coin balance is less than of payment",
-				"coin balance is less than of payment"
-			);
-		}
+    // const userCoin = await getLastCoin(user.user._id);
+    // if (userCoin < body.coin) {
+    // 	return callbackResponse(
+    // 		2004,
+    // 		{},
+    // 		"coin balance is less than of payment",
+    // 		"coin balance is less than of payment"
+    // 	);
+    // }
 
-		const createRequest = await createOtpPaymentRequest(body);
-		await sendWa(
-			createRequest?.phoneNumber || "",
-			`${createRequest?.coin} coin akan anda gunakan untuk pembayaran transaksi pada outlet ${outlet.name}, untuk melanjutkan transaksi ini silahkan berikan code ${createRequest?.code} ini kepada Kasir kami, Terima kasih`
-		);
-		return callbackResponse(1000, createRequest, "SUCCESS", "SUCCESS");
-	} catch (error) {
-		console.log("ERR", error);
-		return callbackResponse(2100, {}, "error by system", "error by system");
-	}
+    // const createRequest = await createOtpPaymentRequest(body);
+    // await sendWa(
+    // 	createRequest?.phoneNumber || "",
+    // 	`${createRequest?.coin} coin akan anda gunakan untuk pembayaran transaksi pada outlet ${outlet.name}, untuk melanjutkan transaksi ini silahkan berikan code ${createRequest?.code} ini kepada Kasir kami, Terima kasih`
+    // );
+    // return callbackResponse(1000, createRequest, "SUCCESS", "SUCCESS");
+    return callbackResponse(
+      4000,
+      {},
+      "system has been stoped",
+      "system has been stoped"
+    );
+  } catch (error) {
+    console.log("ERR", error);
+    return callbackResponse(2100, {}, "error by system", "error by system");
+  }
 };
 
 const validate = async (body: any) => {
-	const v = new Validator();
-	const schema = {
-		coin: { type: "number" },
-		outletId: { type: "string" },
-		phoneNumber: { type: "string" },
-	};
-	const check = v.compile(schema);
-	return await check(body);
+  const v = new Validator();
+  const schema = {
+    coin: { type: "number" },
+    outletId: { type: "string" },
+    phoneNumber: { type: "string" },
+  };
+  const check = v.compile(schema);
+  return await check(body);
 };
--- src/sls-cashier-verified-payment.ts
+++ src/sls-cashier-verified-payment.ts
@@ -1,4 +1,8 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
 import { connect } from "../handler/mongooseConnection";
 import callbackResponse from "../handler/callback";
 import getLastCoin from "../handler/helper/getLastCoin";
@@ -9,52 +13,61 @@ import verifyPayment from "../handler/helper/verifyPaymentCashier";
 import otpPaymentRequestVerified from "../handler/helper/updateOtpPaymentToVerified";
 import DailyUser from "../handler/schemas/dailyUser";
 
-export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
-	context.callbackWaitsForEmptyEventLoop === false;
-	await connect(process.env.DB_NAME || "test");
-	try {
-		console.log("YEAY");
-		const body = event.body ? JSON.parse(event.body) : null;
-		console.log("BODY", body);
-		
-		if (!body || !body.code || !body.outletId) {
-			return callbackResponse(2000, {}, "body request is required", "body request is required");
-		}
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context
+) => {
+  context.callbackWaitsForEmptyEventLoop === false;
+  //   await connect(process.env.DB_NAME || "test");
+  try {
+    // console.log("YEAY");
+    // const body = event.body ? JSON.parse(event.body) : null;
+    // console.log("BODY", body);
 
-		const request = await getOtpPaymentToVerified(body.code, body.phoneNumber);
-		if (!request) {
-			return callbackResponse(2001, {}, "code not found", "code not found");
-		}
+    // if (!body || !body.code || !body.outletId) {
+    // 	return callbackResponse(2000, {}, "body request is required", "body request is required");
+    // }
 
-		const user = await getUserByPhone(request.phoneNumber);
-		if (!user) {
-			return callbackResponse(2002, {}, "phone number not registered", "phone number not registered");
-		}
+    // const request = await getOtpPaymentToVerified(body.code, body.phoneNumber);
+    // if (!request) {
+    // 	return callbackResponse(2001, {}, "code not found", "code not found");
+    // }
 
-		if(user.user.phone.verified === false){
-			await DailyUser.findByIdAndUpdate({_id: user.user._id}, {"phone.verified": true}, {new: true, runValidators: true})
-		}
+    // const user = await getUserByPhone(request.phoneNumber);
+    // if (!user) {
+    // 	return callbackResponse(2002, {}, "phone number not registered", "phone number not registered");
+    // }
 
-		const outlet = await getOutletById(body.outletId);
-		if (!outlet) {
-			return callbackResponse(2003, {}, "outlet not found", "outlet not found");
-		}
+    // if(user.user.phone.verified === false){
+    // 	await DailyUser.findByIdAndUpdate({_id: user.user._id}, {"phone.verified": true}, {new: true, runValidators: true})
+    // }
 
-		const userCoin = await getLastCoin(user.user._id);
-		if (userCoin < request.coin) {
-			return callbackResponse(
-				2004,
-				{},
-				"coin balance is less than of payment",
-				"coin balance is less than of payment"
-			);
-		}
+    // const outlet = await getOutletById(body.outletId);
+    // if (!outlet) {
+    // 	return callbackResponse(2003, {}, "outlet not found", "outlet not found");
+    // }
 
-		const verify = await verifyPayment(request.coin, userCoin, user.user._id.toString(), body.code, body.outletId);
-		await otpPaymentRequestVerified(request._id.toString());
-		return callbackResponse(1000, verify, "SUCCESS", "SUCCESS");
-	} catch (error) {
-		console.log("ERR", error);
-		return callbackResponse(2100, {}, "error by system", "error by system");
-	}
+    // const userCoin = await getLastCoin(user.user._id);
+    // if (userCoin < request.coin) {
+    // 	return callbackResponse(
+    // 		2004,
+    // 		{},
+    // 		"coin balance is less than of payment",
+    // 		"coin balance is less than of payment"
+    // 	);
+    // }
+
+    // const verify = await verifyPayment(request.coin, userCoin, user.user._id.toString(), body.code, body.outletId);
+    // await otpPaymentRequestVerified(request._id.toString());
+    // return callbackResponse(1000, verify, "SUCCESS", "SUCCESS");
+    return callbackResponse(
+      4000,
+      {},
+      "system has been stoped",
+      "system has been stoped"
+    );
+  } catch (error) {
+    console.log("ERR", error);
+    return callbackResponse(2100, {}, "error by system", "error by system");
+  }
 };
